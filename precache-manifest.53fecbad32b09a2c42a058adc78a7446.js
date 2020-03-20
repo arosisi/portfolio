f6c9fa63d7ab5af7ec97f1c2781469559cceffe8
Tom Nguyen <tom.trinh.nguyen@gmail.com>
2020-03-19 20:55:23 -0400

    Updates

---
+++ precache-manifest.53fecbad32b09a2c42a058adc78a7446.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab259462e1f9b5b4f54024f7648770e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a5da0094c66ce54dd8e6",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "821e77757bdc8eb1d725",
+    "url": "/portfolio/static/js/2.95f39143.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/portfolio/static/js/2.95f39143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5da0094c66ce54dd8e6",
+    "url": "/portfolio/static/js/main.73b57059.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
