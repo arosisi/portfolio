a2b5df2ac380873195e7d4c40b482f2b13fb755d
Tom Nguyen <tom.trinh.nguyen@gmail.com>
2020-03-20 21:33:43 -0400

    routing using BrowserRouter

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
 class AppRouter extends React.Component {
   render() {
     return (
-      <HashRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     );
   }
 }
